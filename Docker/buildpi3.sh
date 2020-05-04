24466e19891e30a8936bdfa120e2fb2922cc4bef
Michael <punky@linux-dell-9010.int.bit2atom.de>
2020-05-04 22:48:09 +0200

    notthing work

---
+++ Docker/buildpi3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --tag gui4womo --file arm32v7.Dockerfile_pi3 . 
---
+++ Docker/startpi3.sh
@@ -0,0 +1,2 @@
+#docker run -d -e DISPLAY -v $HOME:/home/pi -v /tmp/.X11-unix/:/tmp/.X11-unix:rw gui4womo gui4womo
+docker run -t -i gui4womo
